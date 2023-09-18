e8c6a6b4a9f38201935e74df155f3fbb0dc07a7c
Aarushi <aarushisinha2005@gmail.com>
2023-09-19 00:27:14 +0530

    hate you git

--- Screenshots_Application/ss_app/views.py
+++ Screenshots_Application/ss_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,  redirect
+import random
+import pyautogui
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth import authenticate, logout, login, get_user_model
 
-# Create your views here.
+def index(request):
+    if request.method == "POST":
+        ss = pyautogui.screenshot()
+        img = f'myimg{random.randint(1000,9999)}.png'
+        ss.save(settings.MEDIA_ROOT/img)
+        messages.success(request,'screenshot has been taken')
+        return render(request,'index.html',{'img':img})
+    return render(request,'index.html')
+
