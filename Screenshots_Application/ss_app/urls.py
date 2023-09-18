ce4be000c31105f6c783b146670e5bac6b339d69
Aarushi <aarushisinha2005@gmail.com>
2023-09-19 00:49:20 +0530

    Signup page and database setup

--- Screenshots_Application/ss_app/urls.py
+++ Screenshots_Application/ss_app/urls.py
@@ -4,4 +4,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('',views.index, name="index"),
+    path('signup',views.signup,name='Signup'),
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
