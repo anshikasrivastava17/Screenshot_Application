604c919989dd40e0db1af84d469042acb73505ac
Aarushi <aarushisinha2005@gmail.com>
2023-09-20 21:19:28 +0530

    setup of home page

--- Screenshots_Application/ss_app/urls.py
+++ Screenshots_Application/ss_app/urls.py
@@ -3,6 +3,7 @@ from .import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('',views.index, name="index"),
+    path('',views.index1, name="home"),
+    path('ss',views.index, name="ss"),
     path('signup',views.signup,name='Signup'),
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
