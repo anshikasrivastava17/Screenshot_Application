feaaa80e0328ef2b194167d059652f4e3997f3aa
anshikasrivastava17 <anshikasrivastava561@gmail.com>
2023-09-20 23:32:01 +0530

    Added time lag for screenshot

--- Screenshots_Application/ss_app/views.py
+++ Screenshots_Application/ss_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,  redirect
 import random
+import time
 import pyautogui
 from django.conf import settings
 from django.contrib import messages
@@ -7,11 +8,13 @@ from django.contrib.auth import authenticate, logout, login, get_user_model
 
 def index(request):
     if request.method == "POST":
+        time.sleep(3)
         ss = pyautogui.screenshot()
         img = f'myimg{random.randint(1000,9999)}.png'
         ss.save(settings.MEDIA_ROOT/img)
         messages.success(request,'screenshot has been taken')
         return render(request,'index.html',{'img':img})
+    
     return render(request,'index.html')
 
 def signup(request):
